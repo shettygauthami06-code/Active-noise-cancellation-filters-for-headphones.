781c7acd98bcba1f27b928bdafcdf4167cf75d7b
gloria26fernandes <gloria26fernandes@gmail.com>
2026-05-12 13:31:15 +0530

    Update Delay line.v

--- testbench/Delay line.v
+++ testbench/Delay line.v
@@ -6,28 +6,28 @@ module tb_delay_line;
     reg rst;
     reg signed [15:0] x_in;
 
-  wire signed [127:0] x_out;                                                   // Flattened output (8 taps × 16 bits = 128 bits)
+wire signed [127:0] x_out;                                                   // Flattened output (8 taps × 16 bits = 128 bits)
                                   
-    delay_line #(.TAPS(8)) uut ( .clk(clk),.rst(rst),.x_in(x_in),.x_out(x_out));                 // Instantiate DUT
+delay_line #(.TAPS(8)) uut ( .clk(clk),.rst(rst),.x_in(x_in),.x_out(x_out));                 // Instantiate DUT
        
 
-   always #5 clk = ~clk;                                                           // Clock generation (10ns period)
+always #5 clk = ~clk;                                                           // Clock generation (10ns period)
 
-   initial 
-       begin
-        clk = 0;
-        rst = 1;
-        x_in = 0;
-        #10 rst = 0;                                                                         // Release reset
+initial 
+     begin
+    clk = 0;
+    rst = 1;
+    x_in = 0;
+    #10 rst = 0;                                                                         // Release reset
 
-        #10 x_in = 1;                                                                       // Apply input sequence
-        #10 x_in = 2;
-        #10 x_in = 3;
-        #10 x_in = 4;
-        #10 x_in = 5;
+     #10 x_in = 1;                                                                       // Apply input sequence
+     #10 x_in = 2;
+     #10 x_in = 3;
+     #10 x_in = 4;
+     #10 x_in = 5;
 
-        #50;
-        $finish;
+    #50;
+    $finish;
     end
 
     always @(posedge clk) begin                                                                                `    // Display outputs every clock                                            
